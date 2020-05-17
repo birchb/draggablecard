02f8ceefa278e038f63ba1f0002816b925d3a085
birchb <b.browning@csuohio.edu>
2020-05-17 18:02:09 -0400

    refactored card to own component close in card emits toggleDialogVis

---
+++ src/components/DraggableCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-card :style="cardStyle" style="max-width: 25vw">
+    <q-bar class="bg-primary text-white" v-touch-pan.mouse="onPan">
+      <div>Draggable zone</div>
+      <q-space></q-space>
+      <q-btn dense flat icon="close" @click="closeSelf()"></q-btn>
+    </q-bar>
+
+    <q-card-section>
+      <div>Dialog content 1</div>
+      <div>Dialog content 2</div>
+      <div>Dialog content 3</div>
+      <div>Dialog content 4</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'DraggableCard',
+  data () {
+    return {
+      cardPos: {
+        x: 0,
+        y: 0
+      }
+    }
+  },
+  computed: {
+    cardStyle () {
+      return {
+        transform: `translate(${this.cardPos.x}px, ${this.cardPos.y}px)`
+      }
+    }
+  },
+  methods: {
+    closeSelf () {
+      this.$emit('toggleDialogVis')
+    },
+    onPan (evt) {
+      this.cardPos = {
+        x: this.cardPos.x + evt.delta.x,
+        y: this.cardPos.y + evt.delta.y
+      }
+    }
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,51 +3,28 @@
     <div id="q-app">
       <div class="q-pa-md q-gutter-sm">
         <q-btn label="Draggable Card" color="primary" @click="dialogVis = true"></q-btn>
-
-        <q-card :style="cardStyle" v-if="dialogVis" style="max-width: 25vw">
-          <q-bar class="bg-primary text-white" v-touch-pan.mouse="onPan">
-            <div>Draggable zone</div>
-            <q-space></q-space>
-            <q-btn dense flat icon="close" @click="dialogVis = false"></q-btn>
-          </q-bar>
-
-          <q-card-section>
-            <div>Dialog content 1</div>
-            <div>Dialog content 2</div>
-            <div>Dialog content 3</div>
-            <div>Dialog content 4</div>
-          </q-card-section>
-        </q-card>
+        <draggable-card v-if="dialogVis" @toggleDialogVis="toggleDataVis" />
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
+
+import DraggableCard from '../components/DraggableCard'
 export default {
   name: 'PageIndex',
-  data() {
+  components: {
+    DraggableCard
+  },
+  data () {
     return {
       dialogVis: false,
-      cardPos: {
-        x: 0,
-        y: 0
-      }
-    }
-  },
-  computed: {
-    cardStyle() {
-      return {
-        transform: `translate(${this.cardPos.x}px, ${this.cardPos.y}px)`
-      }
     }
   },
   methods: {
-    onPan(evt) {
-      this.cardPos = {
-        x: this.cardPos.x + evt.delta.x,
-        y: this.cardPos.y + evt.delta.y
-      }
+    toggleDataVis () {
+      this.dialogVis = !this.dialogVis
     }
   }
 }
